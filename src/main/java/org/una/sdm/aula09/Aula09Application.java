4992e8e28925646c2cd16f9d742f8ccade1df439
malopes21 <malopes21@gmail.com>
2020-11-19 21:51:36 -0300

    ajustes

--- src/main/java/org/una/sdm/aula09/Aula09Application.java
+++ src/main/java/org/una/sdm/aula09/Aula09Application.java
@@ -2,8 +2,10 @@ package org.una.sdm.aula09;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class Aula09Application {
 
 	public static void main(String[] args) {
